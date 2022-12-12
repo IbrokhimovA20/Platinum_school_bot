dcc0def1e148ed102634e63e440a4fbea69171be
Akhrorbek Ibrokhimov <ibrokhimovakhrorbek20@gmail.com>
2022-12-12 18:37:35 +0500

    Added registering students feature

--- handlers/users/users_handler.py
+++ handlers/users/users_handler.py
@@ -81,8 +81,34 @@ async def check_answers(message: Message, state=FSMContext):
     num = user_datas.get("phonenum")
     phonenum_parent = user_datas.get("phonenum_parent")
     subject = user_datas.get("subject")
-    print(name, num, phonenum_parent, subject)
+    if os.path.exists("users.pickle") and os.path.exists("new_users.pickle"):
+        users = pd.read_pickle("users.pickle")
+        all_users = pd.read_pickle("new_users.pickle")
+        if num in list(users['phone_number']):
+            if users[users['phone_number'] == num]:
+                users['Math'] = np.where(users['phone_number'] == num, True, users['Math'])
+        elif num in list(all_users['phone_number']):
+            if phonenum_parent in list(all_users['phone_number']):
+                id = all_users[all_users['phone_number'] == num]['id'][0]
+                parent_id = all_users[all['phone_number'] == phonenum_parent]['id'][0]
+                users.loc[len(users.index)] = [name, num,phonenum_parent, id,parent_id, True, False, False, False]
 
+    elif os.path.exists("new_users.pickle"):
+        new_users = pd.read_pickle("new_users.pickle")
+        if num in list(new_users['phone_number']):
+            if phonenum_parent in list(new_users['phone_number']):
+                id = new_users[new_users['phone_number'] == num]['id'][0]
+                parent_id = new_users[new_users['phone_number'] == phonenum_parent]['id'][0]
+                users_info = {"name":name, "phone_number":num,"parent_number":phonenum_parent, "student_id" : id, "parent_id":parent_id, "Math":True, "Japan" : False, "German":False, "English" : False}
+            else:
+                id = new_users[new_users['phone_number'] == num]['id'][0]
+                print(id)
+                users_info = {"name":name, "phone_number":num,"parent_number":phonenum_parent, "student_id" : id, "parent_id":None, "Math":True, "Japan" : False, "German":False, "English" : False}
+        else:
+            users_info = {"name":name, "phone_number":num,"parent_number":None, "student_id" : None, "parent_id":None, "Math":True, "Japan" : False, "German":False, "English" : False}
+        users_info = pd.DataFrame(users_info, index=[0])
+        print(users_info)
+        users_info.to_pickle("users.pickle")
 
 @dp.message_handler(content_types = ContentType.CONTACT)
 async def check_answers(message: Message):
@@ -95,10 +121,16 @@ async def check_answers(message: Message):
         else:
             new_users = pd.read_pickle('new_users.pickle')
             users.loc[len(users.index)] = [message.contact.phone_number, message.from_user.id]
+        await bot.send_message(chat_id = message.chat.id, text = f"Здравствуйте уважаемый {message.chat.first_name}, добро пожаловать на бот Platinum School! приятного пользования", reply_markup=menu)
+        await message.delete()
     elif os.path.exists("new_users.pickle"):
         users = pd.read_pickle("new_users.pickle")
         users.loc[len(users.index)] = [message.contact.phone_number, message.from_user.id]
+        await bot.send_message(chat_id = message.chat.id, text = f"Здравствуйте уважаемый {message.chat.first_name}, добро пожаловать на бот Platinum School! приятного пользования", reply_markup=menu)
+        await message.delete()
     else:
         users_info = {"phone_number" : message.contact.phone_number,"id" : message.from_user.id}
         users_info = pd.DataFrame(users_info, index=[0])
         users_info.to_pickle('new_users.pickle')
+        await bot.send_message(chat_id = message.chat.id, text = f"Здравствуйте уважаемый {message.chat.first_name}, добро пожаловать на бот Platinum School! приятного пользования", reply_markup=menu)
+        await message.delete()
